e24ff5fcdf90282658845be9608d4fe216b5980f
stevetychen <saobing@vip.qq.com>
2025-03-05 20:10:59 +0800

    feat: update

--- include/rapidjson/internal/clzll.h
+++ include/rapidjson/internal/clzll.h
@@ -48,7 +48,7 @@ inline uint32_t clzll(uint64_t x) {
 #endif // _WIN64
 
     return 63 - r;
-#elif (defined(__GNUC__) && __GNUC__ >= 4) || RAPIDJSON_HAS_BUILTIN(__builtin_clzll)
+#elif (defined(__GNUC__) && __GNUC__ >= 4) || 1
     // __builtin_clzll wrapper
     return static_cast<uint32_t>(__builtin_clzll(x));
 #else
